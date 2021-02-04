ca6b940a1a8be95d70873345c3682f151385698b
ljsimpkin <liamsimpkin@Liams-MacBook-Pro.local>
2021-02-04 12:46:46 +0000

    added groups to my portfolio

--- portfolio/src/js/scripts.js
+++ portfolio/src/js/scripts.js
@@ -35,14 +35,38 @@ for (i = 0; i < projects.length; i++){
 // toggle shuffle on or off by adding &shuffle=off to url
 
 if (getQueryVariable("shuffle") != "off") {
-shuff = shuffle(shuff);
+  shuff = shuffle(shuff);
 }
 
 for (i = 0; i < projects.length; i++) {
-  clon.getElementById("title").innerHTML = projects[shuff[i]]['title'];
-  clon.getElementById("description").innerHTML = projects[shuff[i]]['description'];
-  clon.getElementById("url").href = projects[shuff[i]]['url']
-  clon.getElementById("src").src = projects[shuff[i]]['src']
-  document.getElementById("add_after_me").appendChild(clon);
-  clon = temp.content.cloneNode(true);
+  // if (projects[shuff[i]]['group'] == 'project'){
+    clon.getElementById("title").innerHTML = projects[shuff[i]]['title'];
+    clon.getElementById("description").innerHTML = projects[shuff[i]]['description'];
+    clon.getElementById("url").href = projects[shuff[i]]['url']
+    clon.getElementById("src").src = projects[shuff[i]]['src']
+    clon.getElementById("colour").classList.add(projects[shuff[i]]['group']);
+    // clon.getElementsByClassName("colour").id = "blue"
+    document.getElementById("add_after_me").appendChild(clon);
+    clon = temp.content.cloneNode(true);
+  // }
 }
+
+//toggle buttons for grouping projects, adventure, and learning
+$(document).ready(function(){
+  $(".project-button").click(function(){
+    $(".project").toggleClass("hide");
+    $(".project-button").toggleClass("button-grey");
+  });
+});
+$(document).ready(function(){
+  $(".adventure-button").click(function(){
+    $(".adventure").toggleClass("hide");
+    $(".adventure-button").toggleClass("button-grey");
+  });
+});
+$(document).ready(function(){
+  $(".learning-button").click(function(){
+    $(".learning").toggleClass("hide");
+    $(".learning-button").toggleClass("button-grey");
+  });
+});
